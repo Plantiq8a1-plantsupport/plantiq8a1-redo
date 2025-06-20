2a35a2925c174efc53b7a7fdd9c71f1cbab820f6
Plantiq8a1-plantsupport <aitrongcayservice366972@gmail.com>
2025-06-20 22:13:00 +0700

    main.py

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ def load_model(model_name):
         module = importlib.import_module("model_1")
         return module.PlantExpert()
     elif model_name == "plantiq8a1-vision-lite":
-        raise NotImplementedError("Model 'plantiq8a1-vision-lite' chưa được khai báo.")
+        raise NotImplementedError("Model 'plantiq8a1-deepthink-lite' chưa được khai báo.")
     else:
         raise ValueError(f"Không nhận diện được model: {model_name}")
 
-
 if __name__ == "__main__":
     model = load_model("plantiq8a1-deepthink-lite")
-    print(model.traloicauhoi("Cây cà chua bị vàng lá thì phải làm sao?"))
-
+    cauhoi = "Cây cà chua bị vàng lá thì làm sao khắc phục?"
+    traloi = model.traloicauhoi(cauhoi)
+    print(f"Câu hỏi: {cauhoi}")
+    print(f"Trả lời: {traloi}")
 
 # Plantiq8a1
